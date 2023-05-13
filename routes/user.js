e40d08c23e8cf88eb2b7e99891d5c7a7e21a1089
Tanishqa Arora <tanishqa164@gmail.com>
2023-05-13 23:41:48 +0530

    pagination added

--- routes/user.js
+++ routes/user.js
@@ -8,7 +8,7 @@ router.post('/user', async (req, res) => {
         const user = await new User({
             email: req.body.email
         })
-        // saving usr to the db
+        // saving user to the db
         const userAccount = await user.save();
         res.status(200).send(userAccount);
     }
@@ -19,17 +19,34 @@ router.post('/user', async (req, res) => {
     
 })
 
+// Paginated api
+// getting users page by page
 router.get('/getUsers', async (req, res) => {
     try {
-        const users = await User.find({})
-        // console.log(users.length);
-        res.status(200).send(users);
+        
+        let page = parseInt(req.query.page);          // converting string to number
+        let limit = parseInt(req.query.limit);
+
+        if(!req.query.page || req.query.page === 0) {
+            page = 1;
+        }
+
+        const startIndex = (page - 1) * limit;
+        const endIndex = page * limit;
+        const filteredUsers = await User.find({}).limit(limit).skip(startIndex).exec();
+
+        res.json({
+            page,
+            limit,
+            total: filteredUsers.length,
+            users: filteredUsers
+            
+        })
     }
     catch(error) {
         console.log(error);
         res.send(error);
-    }
-    
+    } 
 })
 
 module.exports = router;
